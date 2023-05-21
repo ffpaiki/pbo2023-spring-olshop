cf6b2ca1a8c84eb2fd711fac25cde243ea98f012
Fridolin Paiki <ff.paiki@unipa.ac.id>
2023-05-22 00:37:25 +0900

    Initial commit

--- src/main/java/com/pbob/lazada/HomeController.java
+++ src/main/java/com/pbob/lazada/HomeController.java
@@ -18,23 +18,7 @@ public class HomeController {
      */
     @GetMapping("/")
     public String home() {
-        // Mengembalikan nama view (halaman web) yang akan ditampilkan, yaitu home.html
+        // Mengembalikan nama view yang akan ditampilkan, yaitu home.html
         return "home";
-    }
-    
-    /*
-     * @GetMapping("/mahasiswa") menandakan bahwa method getMahasiswa() akan dipanggil ketika user mengakses URL http://localhost:8080/mahasiswa
-     * 
-     * Atribut model digunakan untuk mengirimkan data ke view (halaman web)
-     */
-    @GetMapping("/mahasiswa")
-    public String getMahasiswa(Model model){
-        Mahasiswa rahmat = new Mahasiswa(1, "202065002", "Rahmat");
-
-        // Mengirimkan data object 'rahmat' ke view (halaman web). Data tersebut dapat diakses pada view menggunakan nama 'mhs'
-        model.addAttribute("mhs", rahmat);
-
-        // Mengembalikan nama view (halaman web) yang akan ditampilkan, yaitu mahasiswa.html
-        return "mahasiswa";
     }
 }
