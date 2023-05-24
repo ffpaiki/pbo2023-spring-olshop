919ec62a211c801ab46f652fc583068b4dc9d91c
Fridolin Paiki <ff.paiki@unipa.ac.id>
2023-05-24 12:30:04 +0900

    CRUD Template

--- src/main/java/com/pbob/lazada/Product/ProductController.java
+++ src/main/java/com/pbob/lazada/Product/ProductController.java
@@ -1,16 +1,12 @@
 package com.pbob.lazada.Product;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -30,50 +26,32 @@ public class ProductController {
 
     @GetMapping("/")
     public String getAll(Model model) {
-        List<Product> products = this.productService.getAll();
-
-        model.addAttribute("dataProduct", products);
-
         return "product/index";
     }
 
     @GetMapping("/tambah")
     public String form_tambah(Model model) {
-        Product product = new Product();
-
-        model.addAttribute("product", product);
-
         return "product/tambah";
     }
 
     @PostMapping("/")
     public String simpan(@ModelAttribute Product product) {
-        this.productService.save(product);
-
         return "redirect:/product/";
     }
 
     @GetMapping("/edit/{id}")
     public String form_edit(@PathVariable Long id, Model model) {
 
-        Product product = this.productService.getById(id);
-
-        model.addAttribute("product", product);
-
         return "product/edit";
     }
 
     @PostMapping("/update/{id}")
     public String update(@PathVariable Long id, @ModelAttribute Product product) {
-
-        this.productService.update( id, product);
         return "redirect:/product/";
     }
 
     @GetMapping("/hapus/{id}")
     public String hapus(@PathVariable Long id) {
-
-        this.productService.delete(id);
         return "redirect:/product/";
     }
 }
--- src/main/java/com/pbob/lazada/Product/ProductService.java
+++ src/main/java/com/pbob/lazada/Product/ProductService.java
@@ -1,8 +1,5 @@
 package com.pbob.lazada.Product;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,39 +7,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ProductService {
-    private final ProductRepository productRepository;
-
-    @Autowired
-    public ProductService(ProductRepository productRepository){
-        this.productRepository = productRepository;
-    }
-
-    public List<Product> getAll(){
-        return productRepository.findAll();
-    }
-
-    public Product save(Product product){
-        return productRepository.save(product);
-    }
-
-    public Product getById(Long id){
-        return productRepository.findById(id).orElse(null);
-    }
-
-    public void delete(Long id){
-        productRepository.deleteById(id);
-    }
-
-    public Product update(Long id, Product product){
-        Product product_lama = productRepository.findById(id).orElse(null);
-
-        product_lama.setNama(product.getNama());
-        product_lama.setDeskripsi(product.getDeskripsi());
-        product_lama.setBrand(product.getBrand());
-        product_lama.setStok(product.getStok());
-        product_lama.setHarga(product.getHarga());
-
-        return productRepository.save(product_lama);
-    }
+    
     
 }
