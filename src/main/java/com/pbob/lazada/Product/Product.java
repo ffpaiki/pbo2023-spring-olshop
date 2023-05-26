39649ba68fc9585407a6bc06cc39e8f12ec2cb57
Fridolin Paiki <ff.paiki@unipa.ac.id>
2023-05-26 13:04:40 +0900

    Update ProductController and ProductService

--- src/main/java/com/pbob/lazada/Product/Product.java
+++ src/main/java/com/pbob/lazada/Product/Product.java
@@ -1,22 +1,30 @@
 package com.pbob.lazada.Product;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
  * Product
+ * @Data digunakan untuk mengenerate getter dan setter secara otomatis
+ * @Entity digunakan untuk menyatakan bahwa class Product adalah sebuah entitas 
+ * (tabel pada database akan dihasilkan secara otomatis berdasarkan data pada class dengan anotasi @Entity)
  */
 @Data
 @Entity
 public class Product {
 
+    /*
+     * @Id menyatakan bahwa atribut id akan digunakan ID pada entitas/class Product
+     * @GeneratedValue menyatakann bahwa atribut id akan di generate secara otomatis dengan strategi yang telah ditentukan
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nama;
     private String deskripsi;
     private String brand;
--- src/main/java/com/pbob/lazada/Product/ProductController.java
+++ src/main/java/com/pbob/lazada/Product/ProductController.java
@@ -5,16 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * ProductController
+ * @Controller digunakan untuk menandakan bahwa class ProductController adalah sebuah controller
  */
 @Controller
 @RequestMapping(path = "/product")
@@ -22,6 +21,9 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /*
+     * @Autowired digunakan untuk menginject dependency secara otomatis
+     */
     @Autowired
     public ProductController(ProductService productService){
         this.productService = productService;
